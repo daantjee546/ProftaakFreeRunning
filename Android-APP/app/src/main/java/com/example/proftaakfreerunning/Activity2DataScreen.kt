0f152227cf54fbbec457c8cfee53d614cd603043
daantjee546 <50410354+daantjee546@users.noreply.github.com>
2019-11-25 15:19:00 +0100

    add fix data screen

--- Android-APP/app/src/main/java/com/example/proftaakfreerunning/Activity2DataScreen.kt
+++ Android-APP/app/src/main/java/com/example/proftaakfreerunning/Activity2DataScreen.kt
@@ -2,11 +2,28 @@ package com.example.proftaakfreerunning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class Activity2DataScreen : AppCompatActivity() {
+
+    var lv: ListView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activity2_data_screen)
+
+        lv = this.findViewById(R.id.lvSectorTimes)
+
+        val list = ArrayList<String>()
+        list.add("Sector 1 = 3 seconden")
+        list.add("Sector 2 = 3 seconden")
+        list.add("Sector 3 = 3 seconden")
+        list.add("Sector 4 = 3 seconden")
+        list.add("Sector 5 = 3 seconden")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        lv?.adapter = adapter
     }
 }
--- Android-APP/app/src/main/java/com/example/proftaakfreerunning/MainActivity.kt
+++ Android-APP/app/src/main/java/com/example/proftaakfreerunning/MainActivity.kt
@@ -2,6 +2,13 @@ package com.example.proftaakfreerunning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buttonclickLogin(view: View)
+    {
+        startActivity(Intent(this@MainActivity, Activity2DataScreen::class.java))
+    }
+
+    fun buttonclickRegister(view: View)
+    {
+        startActivity(Intent(this@MainActivity, Activity3RegisterScreen::class.java))
+    }
+
 }
