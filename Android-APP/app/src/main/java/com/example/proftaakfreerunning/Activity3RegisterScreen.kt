fd3ba9390b6b1a5a7e45c668255c9d68a1b6fdda
daantjee546 <50410354+daantjee546@users.noreply.github.com>
2019-11-25 14:57:36 +0100

    Add design register screen

---
+++ Android-APP/app/src/main/java/com/example/proftaakfreerunning/Activity3RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.proftaakfreerunning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity3RegisterScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity3_register_screen)
+    }
+}
