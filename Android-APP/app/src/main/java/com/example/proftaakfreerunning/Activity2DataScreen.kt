3b1da1ecb81ebcfac911b972ab51677125296990
daantjee546 <50410354+daantjee546@users.noreply.github.com>
2019-11-25 14:48:50 +0100

    Add design for data screen

---
+++ Android-APP/app/src/main/java/com/example/proftaakfreerunning/Activity2DataScreen.kt
@@ -0,0 +1,12 @@
+package com.example.proftaakfreerunning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2DataScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity2_data_screen)
+    }
+}
